4fcfc044635dccf5f552c0e44914f623b71bb05c
Andeo1812 <alexanforall@mail.ru>
2022-09-04 15:54:09 +0300

    add types, arifmetic

---
+++ project/scripts/TypeConversion.js
@@ -0,0 +1,39 @@
+//  Для работы кода в современном режиме
+"use strict"
+
+
+let value = true;
+alert(typeof value); //  boolean
+
+value = String(value); //  теперь value это строка "true"
+alert(typeof value); //  string
+
+
+//   Численное преобразование происходит в математических функциях и выражениях.
+//  Например, когда операция деления / применяется не к числу:
+alert( "6" / "2" ); //  3, строки преобразуются в числа
+
+
+//  Мы можем использовать функцию Number(value), чтобы явно преобразовать value к числу:
+let str = "123";
+alert(typeof str); // string
+
+let num = Number(str); // становится числом 123
+alert(typeof num); // number
+
+//  Если строка не может быть явно приведена к числу, то результатом преобразования будет NaN. Например:
+let age = Number("Любая строка вместо числа");
+alert(age); // NaN, преобразование не удалось
+
+
+//  Еще примеры:
+alert( Number("   123   ") ); // 123
+alert( Number("123z") );      // NaN (ошибка чтения числа на месте символа "z")
+alert( Number(true) );        // 1
+alert( Number(false) );       // 0
+
+alert( Boolean(1) ); // true
+alert( Boolean(0) ); // false
+
+alert( Boolean("Привет!") ); // true
+alert( Boolean("") ); // false
